d9bd018b72f0111edcc01f99a6a7c9c7e3502f12
Allan Eric Jepsen <2100187@aluno.univesp.br>
2023-09-14 17:10:26 -0300

    Add files via upload

---
+++ acc-csharp-011-exercises-azure-deploy-allan-eric-acc-011-project-exercises-azure-deploy/src/azure-deploy.Test/WebApplication.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace AzureDeploy.Test;
+
+public class WebApplication : WebApplicationFactory<Program>
+{
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        return base.CreateHost(builder);
+    }
+
+     public async Task<HttpResponseMessage> ApiGetRequest(string route)
+    {
+        var client = CreateClient();
+        return await client.GetAsync(route);
+    }
+}
